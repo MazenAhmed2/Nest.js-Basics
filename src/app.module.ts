30b20446c129021c6ddb6e51f30435571920951a
Mazen Ahmed <106814868+MazenAhmed2@users.noreply.github.com>
2025-05-05 11:04:15 +0000

    docker compose file for postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, BookmarkModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, BookmarkModule, AuthModule],
 })
 export class AppModule {}
