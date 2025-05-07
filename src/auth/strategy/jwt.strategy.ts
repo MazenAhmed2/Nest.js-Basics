f7867d1fcbe296f94b1e41108bb0267e96c09da1
Mazen Ahmed <106814868+MazenAhmed2@users.noreply.github.com>
2025-05-07 00:59:23 +0000

    jwt validation implement

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt'){ // jwt is the name of strategy
+    constructor(private config : ConfigService, private prisma : PrismaService){
+        const options = {
+            // ignoreExpiration: false,
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey : config.get('JWT_SECRET'),
+        }
+        super(options)
+    }
+
+    async validate(payload) {
+
+        // fetch user from database
+        const user = await this.prisma.user.findUnique({
+            where : {
+                id : payload.sub
+            }
+        })
+
+        if (user){
+            const {hash : _ , ...result} = user
+            return result
+        }
+
+        return null  // this return will be attached to req.user
+    }
+}
