b234b10e475c7447c9131dcc3a799ef86a65ea98
Mazen Ahmed <106814868+MazenAhmed2@users.noreply.github.com>
2025-05-08 15:54:46 +0000

    adding some tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,8 @@ import { Test } from '@nestjs/testing'
 import { AppModule } from '../src/app.module'
 import { INestApplication, ValidationPipe } from '@nestjs/common'
 import { PrismaService } from '../src/prisma/prisma.service'
+import * as pactum from 'pactum'
+import { AuthDto } from '../src/auth/dto'
 
 describe('APP e2e', ()=>{
   let app : INestApplication
@@ -18,15 +20,56 @@ describe('APP e2e', ()=>{
       }))
     
     await app.init()
+    await app.listen(3333)
 
     // Get the prisma service of the app
     prisma = app.get(PrismaService)
 
     await prisma.cleanDb()
+    pactum.request.setBaseUrl('http://localhost:3333')
   })
 
   afterAll(()=>{
     app.close()
+  })
+
+  const dto : AuthDto = {
+    email: "mazengassar@gmail.com",
+    password: "mazen"
+  }
+
+  describe('Auth', ()=>{
+
+    describe('Signup', ()=>{
+      it('should pass', ()=>{
+        pactum.spec().post(`auth/signup`).withBody(dto).expectStatus(201).inspect()
+      })
+      it('empty email', ()=>{
+        pactum.spec().post(`auth/signup`).withBody({password : dto.password}).expectStatus(400).inspect()
+      })
+      it('empty password', ()=>{
+        pactum.spec().post(`auth/signup`).withBody({email : dto.email}).expectStatus(400).inspect()
+      })
+    })
+
+    describe('Signin', ()=>{
+      it('should pass', ()=>{
+        pactum.spec().post(`auth/signin`).withBody(dto).expectStatus(200).stores('userAt', 'access_token')
+      })
+    })
+  })
+
+  describe('User', ()=>{
+    it('should pass', ()=>{
+      pactum.spec().get(`users/me`).withBearerToken('$S{userAt}').expectStatus(200).stores('userAt', 'access_token')
+    })
+    it('no access token', ()=>{
+      pactum.spec().get(`users/me`).expectStatus(401).stores('userAt', 'access_token')
+    })
+  })
+
+  describe('Bookmark', ()=>{
+
   })
 
   it.todo('test')
